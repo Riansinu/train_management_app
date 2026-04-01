980c5f59ffcc5bea84a0faa6ddb962169649bfbf
Rian <your-email@gmail.com>
2026-04-01 09:34:57 +0530

    Added UC13: Performance comparison using loop vs stream

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -289,5 +289,51 @@ public class TrainManagementApp {
         System.out.println("\nSafety Status: " + (isSafe ? "SAFE" : "UNSAFE"));
 
         System.out.println("\nUC12 safety validation completed...");
+
+
+        // ================= UC13 =================
+        System.out.println("\nUC13 - Performance Comparison (Loops vs Streams)");
+        System.out.println("===================================");
+
+// Create large dataset for testing
+        List<Bogie> largeList = new ArrayList<>();
+
+        for (int i = 0; i < 100000; i++) {
+            largeList.add(new Bogie("Sleeper", 72));
+            largeList.add(new Bogie("AC Chair", 56));
+            largeList.add(new Bogie("First Class", 24));
+        }
+
+// -------- Loop-Based Filtering --------
+        long startLoop = System.nanoTime();
+
+        List<Bogie> loopResult = new ArrayList<>();
+        for (Bogie b : largeList) {
+            if (b.capacity > 60) {
+                loopResult.add(b);
+            }
+        }
+
+        long endLoop = System.nanoTime();
+        long loopTime = endLoop - startLoop;
+
+// -------- Stream-Based Filtering --------
+        long startStream = System.nanoTime();
+
+        List<Bogie> streamResult = largeList.stream()
+                .filter(b -> b.capacity > 60)
+                .toList();
+
+        long endStream = System.nanoTime();
+        long streamTime = endStream - startStream;
+
+// -------- Display Results --------
+        System.out.println("\nLoop Result Size: " + loopResult.size());
+        System.out.println("Stream Result Size: " + streamResult.size());
+
+        System.out.println("\nLoop Execution Time: " + loopTime + " ns");
+        System.out.println("Stream Execution Time: " + streamTime + " ns");
+
+        System.out.println("\nUC13 performance comparison completed...");
     }
 }
