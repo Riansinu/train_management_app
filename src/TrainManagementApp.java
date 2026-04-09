9950852b54c4d1a59f58e1d2b9080dd75efb2788
Riansinu <riansinu3@gmail.com>
2026-04-09 09:25:19 +0530

    Added UC19: Binary search for bogie ID

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -1,24 +1,43 @@
 import java.util.Arrays;
 
-public class TrainManagementApp {
+public class TrainManagementApp  {
 
     public static void main(String[] args) {
 
-        System.out.println("UC17 - Sort Bogie Names using Arrays.sort()");
+        System.out.println("UC19 - Binary Search for Bogie ID");
         System.out.println("===================================");
 
-        // Bogie type names
-        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Pantry"};
+        // Sorted bogie IDs (IMPORTANT for binary search)
+        int[] bogieIds = {101, 102, 103, 104, 105, 106};
 
-        System.out.println("\nBefore Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+        int target = 104;
 
-        // Built-in sorting
-        Arrays.sort(bogieNames);
+        System.out.println("\nBogie IDs: " + Arrays.toString(bogieIds));
+        System.out.println("Searching for: " + target);
 
-        System.out.println("\nAfter Sorting (Alphabetical):");
-        System.out.println(Arrays.toString(bogieNames));
+        // Binary Search Logic
+        int left = 0;
+        int right = bogieIds.length - 1;
+        boolean found = false;
 
-        System.out.println("\nSorting completed successfully...");
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (bogieIds[mid] == target) {
+                System.out.println("Bogie found at index: " + mid);
+                found = true;
+                break;
+            } else if (bogieIds[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Bogie ID not found.");
+        }
+
+        System.out.println("\nBinary Search completed successfully...");
     }
 }
